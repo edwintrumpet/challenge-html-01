197cc1ea00f4ee00e1d659481096e62ba8d0a1a5
edwin <spark.com.co@gmail.com>
2020-02-22 16:02:33 -0500

    home con css grid

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import Layout from '../components/Layout';
+import '../styles/containers/Home.styl';
 
 const Home = () => (
   <Layout>
-    <h1>Layout Wotks!</h1>
+    <div className="home">
+      <div className="home__big-card">1</div>
+      <div className="home__small-card">2</div>
+      <div className="home__big-card">3</div>
+      <div className="home__big-card">4</div>
+      <div className="home__small-card">5</div>
+      <div className="home__big-card">6</div>
+      <div className="home__big-card">7</div>
+      <div className="home__big-card">8</div>
+      <div className="home__small-card">9</div>
+    </div>
   </Layout>
 );
 
